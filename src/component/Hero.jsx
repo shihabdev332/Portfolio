d1cbcd7f78b9c40b9fc7a2e9ba92263dfb053aee
Md. Shihab <shihab.dev332@gmail.com>
2026-01-28 00:58:46 +0600

    Update Hero.jsx

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -195,7 +195,7 @@ const Hero = () => {
               <img
                 src="this.png"
                 alt="Shihab"
-                className="w-[80%] h-full object-cover scale-110 group-hover:scale-105 transition-transform duration-700"
+                className="w-full h-full object-cover mt-[5px] scale-110 group-hover:scale-105 transition-transform duration-700"
               />
             </div>
 
